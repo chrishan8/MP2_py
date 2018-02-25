b11c9b7cc1f28dd1c11f71b58e838ce6b284685e
Chris Han <chris.han8@gmail.com>
2018-02-25 14:38:03 -0500

    fixing initial line split in linkcountmapper

--- LinkCountReducer.py
+++ LinkCountReducer.py
@@ -7,8 +7,11 @@ currentTotal = 0
 # input comes from STDIN
 for line in sys.stdin:
     line = line.strip()
-    page, count = line.split('\t', 1)
-    count = int(count)
+    try:
+        page, count = line.split('\t', 1)
+        count = int(count)
+    except ValueError:
+        pass
 
     if currentPage == page:
         currentTotal += count
