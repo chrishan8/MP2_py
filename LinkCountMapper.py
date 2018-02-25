d636c5466eee6cd9bae1a89480b073e6bc59bcbc
Chris Han <chris.han8@gmail.com>
2018-02-25 14:25:33 -0500

    fixing initial line split in linkcountmapper

--- LinkCountMapper.py
+++ LinkCountMapper.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 for line in sys.stdin:
-    links = filter(None, re.split(r'[ :]', line))
+    links = filter(None, map(lambda x: x.strip(), re.split(r'[ :]', line)))
     links.pop(0)
     for link in links:
         print '%s\t%d' % (link, 1)
