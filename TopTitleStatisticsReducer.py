8ab44cf0fe7f58aa6e5c4649a294bfe15e74b65c
Chris Han <chris.han8@gmail.com>
2018-02-25 02:14:15 -0500

    completed first draft of exercise B

--- TopTitleStatisticsReducer.py
+++ TopTitleStatisticsReducer.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 import sys
 
-
-
-#TODO
+wordsToCounts = []
 
 for line in sys.stdin:
-    # TODO
+    line = line.strip()
+    word, count = line.split('\t', 1)
+    count = int(count)
+    wordsToCounts.append((word, count))
 
-#TODO
+total = 0
+for (word, count) in wordsToCounts:
+    total += count
+average = len(wordsToCounts) / total
+for (word, count) in wordsToCounts:
+    variance += (average - count) ** 2
+variance /= len(wordsToCounts)
+
+print 'Mean\t%d' % average
+print 'Sum\t%d' % total
+print 'Min\t%d' % min(wordsToCounts, key=lambda x: x[1])
+print 'Max\t%d' % max(wordsToCounts, key=lambda x: x[1])
+print 'Var\t%d' % variance
